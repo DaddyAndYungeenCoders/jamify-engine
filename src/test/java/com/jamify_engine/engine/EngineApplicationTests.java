8f484d972adc528884eb41bcb46c98dcfdf32430
Sandro Spina <sandro.spina@sis-id.com>
2024-12-24 14:17:18 +0100

    init microservice

---
+++ src/test/java/com/jamify_engine/engine/EngineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jamify_engine.engine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EngineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
