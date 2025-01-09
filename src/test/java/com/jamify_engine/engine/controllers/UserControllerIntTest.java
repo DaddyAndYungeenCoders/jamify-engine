92fe8547f8be85da0fb612ffa5a9c0fe37fea079
Damien MAILHEBIAU <damienmailhebiau@gmail.com>
2025-01-09 23:24:36 +0100

    fix: user creation logic

--- src/test/java/com/jamify_engine/engine/controllers/UserControllerIntTest.java
+++ src/test/java/com/jamify_engine/engine/controllers/UserControllerIntTest.java
@@ -2,10 +2,17 @@ package com.jamify_engine.engine.controllers;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.jamify_engine.engine.models.dto.UserDTO;
+import com.jamify_engine.engine.models.entities.UserEntity;
+import com.jamify_engine.engine.repository.UserRepository;
 import com.jamify_engine.engine.security.SecurityTestConfig;
+import com.jamify_engine.engine.service.implementations.UserServiceImpl;
+import com.jamify_engine.engine.service.interfaces.UserService;
 import com.jamify_engine.engine.utils.TestsUtils;
+import org.h2.engine.User;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -13,6 +20,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -34,30 +42,69 @@ class UserControllerIntTest {
     @Value("${config.uaa.jamify-engine-api-key}")
     private String correctJamifyEngineApiKey;
 
+    @Autowired
+    private UserService userService;
+
 
     @BeforeEach
     void setUp() {
     }
 
-//    @Test
+    @Test
 //    @WithMockUser
-//    void createUser_withValidUser_shouldReturnTheNewUser() throws Exception {
-//        UserDTO userDto = TestsUtils.buildUserDto();
-//
-//        mockMvc.perform(post("/api/v1/users/uaa/create")
-//                        .contentType("application/json")
-//                        .header("X-API-KEY", correctJamifyEngineApiKey)
-//                        .content(new ObjectMapper().writeValueAsString(userDto)))
-//
-//                .andExpect(status().isOk())
-//                .andExpect(jsonPath("$.name").value(userDto.name()))
-//                .andExpect(jsonPath("$.email").value(userDto.email()))
-//                .andExpect(jsonPath("$.country").value(userDto.country()))
-//                .andExpect(jsonPath("$.provider").value(userDto.provider()))
-//                .andExpect(jsonPath("$.imgUrl").value(userDto.imgUrl()))
-//                .andExpect(jsonPath("$.userProviderId").value(userDto.userProviderId()))
-//                .andExpect(jsonPath("$.jams").value(userDto.jams()));
-//    }
+    void createUser_withValidUser_shouldReturnTheNewUser() throws Exception {
+        UserDTO userDto = TestsUtils.buildUserDto();
+
+        mockMvc.perform(post("/api/v1/users/uaa/create")
+                        .contentType("application/json")
+                        .header("X-API-KEY", correctJamifyEngineApiKey)
+                        .content(new ObjectMapper().writeValueAsString(userDto)))
+
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.name").value(userDto.name()))
+                .andExpect(jsonPath("$.email").value(userDto.email()))
+                .andExpect(jsonPath("$.country").value(userDto.country()))
+                .andExpect(jsonPath("$.provider").value(userDto.provider()))
+                .andExpect(jsonPath("$.imgUrl").value(userDto.imgUrl()))
+                .andExpect(jsonPath("$.userProviderId").value(userDto.userProviderId()))
+                .andExpect(jsonPath("$.jams").value(userDto.jams()));
+    }
+
+    @Test
+//    @WithMockUser
+    void createUser_withValidUserAndNewProvider_shouldReturnTheUpdatedNewUser() throws Exception {
+        UserDTO userDto = TestsUtils.buildUserDto();
+
+        UserDTO updatedUserDto = UserDTO.builder()
+                .name(userDto.name())
+                .email(userDto.email())
+                .country(userDto.country())
+                .provider("newProvider")
+                .imgUrl("http://example")
+                .userProviderId("11111111")
+                .jams(userDto.jams())
+                .build();
+
+        mockMvc.perform(post("/api/v1/users/uaa/create")
+                        .contentType("application/json")
+                        .header("X-API-KEY", correctJamifyEngineApiKey)
+                        .content(new ObjectMapper().writeValueAsString(updatedUserDto)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.name").value(updatedUserDto.name()))
+                .andExpect(jsonPath("$.email").value(updatedUserDto.email()))
+                .andExpect(jsonPath("$.country").value(updatedUserDto.country()))
+                .andExpect(jsonPath("$.provider").value("newProvider"))
+                .andExpect(jsonPath("$.imgUrl").value("http://example"))
+                .andExpect(jsonPath("$.userProviderId").value("11111111"))
+                .andExpect(jsonPath("$.jams").value(updatedUserDto.jams()));
+
+        UserEntity updatedUser = userService.findEntityByEmail(updatedUserDto.email());
+
+        assert updatedUser != null;
+        Assertions.assertEquals(updatedUser.getProvider(), "newProvider");
+        Assertions.assertEquals(updatedUser.getImgUrl(), "http://example");
+        Assertions.assertEquals(updatedUser.getUserProviderId(), "11111111");
+    }
 
     @Test
     @WithMockUser
