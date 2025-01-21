d900580d12ecf997fdfbc7da4a4d69dfad0618a1
Sandro SPINA <75906608+SandroSpina698@users.noreply.github.com>
2025-01-21 11:32:55 +0100

    Add id dtos (#26)
    * add id to jam dto
    
    * fix jam launching
    
    * fix pipeline
    
    * big FIX
    
    * user providerid
    
    * fix conflicts
    
    ---------
    
    Co-authored-by: SandroSpina698 <sandro.spina.pro@gmail.com>

--- src/main/java/com/jamify_engine/engine/repository/UserRepository.java
+++ src/main/java/com/jamify_engine/engine/repository/UserRepository.java
@@ -3,6 +3,8 @@ package com.jamify_engine.engine.repository;
 import com.jamify_engine.engine.models.entities.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<UserEntity, Long> {
     /**
      * Checks if a user exists by their email.
@@ -19,4 +21,11 @@ public interface UserRepository extends JpaRepository<UserEntity, Long> {
      * @return the UserEntity with the given email, or null if no user is found
      */
     UserEntity findByEmail(String email);
+
+    /**
+     * Find a user from its provider id
+     * @param userProviderId -
+     * @return The corresponding user
+     */
+    Optional<UserEntity> findByUserProviderId(String userProviderId);
 }
