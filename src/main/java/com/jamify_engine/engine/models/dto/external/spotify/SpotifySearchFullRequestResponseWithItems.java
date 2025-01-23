551fc6ad891dc23a8ebf740558652d9f009a61b7
SandroSpina698 <sandro.spina.pro@gmail.com>
2025-01-23 17:25:23 +0100

    fix / search object with itemS

---
+++ src/main/java/com/jamify_engine/engine/models/dto/external/spotify/SpotifySearchFullRequestResponseWithItems.java
@@ -0,0 +1,14 @@
+package com.jamify_engine.engine.models.dto.external.spotify;
+
+import lombok.*;
+
+import java.util.List;
+
+@Builder
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class SpotifySearchFullRequestResponseWithItems {
+    List<SpotifyTrackDTO> items;
+}
